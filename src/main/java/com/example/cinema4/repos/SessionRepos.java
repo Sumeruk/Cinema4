316c7ccb4de2b84f362c0c61fc7a0e6d65df4f01
Kostya Zinchenko <zinchenkokostantin@yandex.ru>
2023-10-30 23:02:03 +0300

    initial commit

--- src/main/java/com/example/cinema4/repos/SessionRepos.java
+++ src/main/java/com/example/cinema4/repos/SessionRepos.java
@@ -1,6 +1,5 @@
 package com.example.cinema4.repos;
 
-import com.example.cinema4.entity.Film;
 import com.example.cinema4.entity.Session;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
